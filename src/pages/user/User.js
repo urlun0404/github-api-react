8b7d16b7de60574a4d8560f6eaf655acb9e9569c
urlun0404 <urlun0404@gmail.com>
2022-03-30 16:49:51 +0800

    Get user's repositories throught calling GitHub REST API User component stores all the repositories of the requested user and passes data to its subcomponents Header and Repo.

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,14 +1,28 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "./components/Header";
 import Repo from "./components/Repo";
 
 const User = () => {
   const { username } = useParams();
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  let [userRepos, setUserRepos] = useState([]);
+
+  const getUserRepos = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setUserRepos([...userRepos, ...data]);
+  };
+
+  useEffect(() => {
+    getUserRepos(url);
+  }, []);
 
   return (
     <div>
       <Header />
-      <Repo />
+      <Repo userRepos={userRepos} />
     </div>
   );
 };
