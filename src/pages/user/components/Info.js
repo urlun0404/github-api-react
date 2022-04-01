df684aa0b5ee7c0bfe897d3ff3e6ce839a7fd7bc
urlun0404 <urlun0404@gmail.com>
2022-04-01 15:02:03 +0800

    Modify the layout css shown in User's repository list page

--- src/pages/user/components/Info.js
+++ src/pages/user/components/Info.js
@@ -1,6 +1,6 @@
 const Info = ({ repoInfo }) => {
   return (
-    <main>
+    <main className="repos">
       <div className="row">
         <div className="col-4">fullname</div>
         <div className="col-6">{repoInfo.full_name}</div>
@@ -13,6 +13,7 @@ const Info = ({ repoInfo }) => {
         <div className="col-4">stars</div>
         <div className="col-6">{repoInfo.stargazers_count}</div>
       </div>
+      <div className="background"></div>
     </main>
   );
 };
